e3cbc6b06eff20210a08643360c48ecc61471f00
Samir Sayyad <samir.saiad@gmail.com>
2020-06-25 18:06:19 +0430

    bang going to remove

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ exports.eejsBlock_editbarMenuRight = function (hook_name, args, cb) {
 	var str = args.content;
 
 	str = str.replace(/\n/g, "<newline>");
-	str = str.replace(/<li data-type="button" data-key="savedRevision">/, "");
+	str = str.replace(/<li data-type="button" data-key="savedRevision">/, `<li data-type="button" style="display:none" data-key="savedRevision">`);
 	str = str.replace(/<newline>/g, '\n');
 	args.content = str;
   return cb();
